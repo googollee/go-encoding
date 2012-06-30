76f015e00bc25dbd2bfc579aeccf7440760dd974
Googol Lee <googollee@gmail.com>
2012-06-30 21:39:32 +0800

    add quoted-printable encoder

--- quoted_printable.go
+++ quoted_printable.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func UnHex(c byte) (byte, bool) {
+func unHex(c byte) (byte, bool) {
 	if byte('0') <= c && c <= byte('9') {
 		return c - byte('0'), true
 	}
@@ -18,6 +18,22 @@ func UnHex(c byte) (byte, bool) {
 	return 0, false
 }
 
+func hex(p byte) (ret [2]byte) {
+	ret[0] = p / 16
+	if ret[0] >= 10 {
+		ret[0] = ret[0] - 10 + byte('A')
+	} else {
+		ret[0] = ret[0] + byte('0')
+	}
+	ret[1] = p % 16
+	if ret[1] >= 10 {
+		ret[1] = ret[1] - 10 + byte('A')
+	} else {
+		ret[1] = ret[1] + byte('0')
+	}
+	return
+}
+
 type decodeStatus int
 
 const (
@@ -28,17 +44,17 @@ const (
 )
 
 type decoder struct {
-	r io.Reader
+	r      io.Reader
 	status decodeStatus
-	temp byte
+	temp   byte
 }
 
 func NewQuotedPrintableDecoder(r io.Reader) io.Reader {
 	return &decoder{
-		r: r,
+		r:      r,
 		status: decodeNormal,
-		temp: 0,
-	}	
+		temp:   0,
+	}
 }
 
 func (d *decoder) Read(p []byte) (n int, err error) {
@@ -54,17 +70,17 @@ func (d *decoder) Read(p []byte) (n int, err error) {
 			if b == byte('=') {
 				d.status = decodeQuoted
 			} else {
-				p[n] = b 
+				p[n] = b
 				n++
 			}
 		case decodeQuoted:
 			switch b {
-			case byte('\n'): 
+			case byte('\n'):
 				d.status = decodeNormal
 			case byte('\r'):
 				d.status = decodeReturn
 			default:
-				h, ok := UnHex(b)
+				h, ok := unHex(b)
 				if !ok {
 					err = fmt.Errorf("can't convert %c(%d) to hex", rune(b), b)
 					return
@@ -79,7 +95,7 @@ func (d *decoder) Read(p []byte) (n int, err error) {
 			}
 			d.status = decodeNormal
 		case decodeFirst:
-			h, ok := UnHex(b)
+			h, ok := unHex(b)
 			if !ok {
 				err = fmt.Errorf("can't convert %c(%d) to hex", rune(b), b)
 				return
@@ -91,4 +107,198 @@ func (d *decoder) Read(p []byte) (n int, err error) {
 		}
 	}
 	return
+}
+
+type encodeStatus int
+
+const (
+	encodeNormal encodeStatus = iota
+	encodeSpace
+	encodeSpaceReturn
+	encodeReturn
+)
+
+const maxBuf = 1024
+
+type encoder struct {
+	w             io.Writer
+	status        encodeStatus
+	maxLineLength int
+	lineLength    int
+	nbuf          int
+	buf           [maxBuf]byte
+	last          byte
+}
+
+func NewQuotedPrintableEncoder(w io.Writer, maxLength int) io.WriteCloser {
+	if maxLength < 3 {
+		maxLength = 3
+	}
+	if maxLength > 76 {
+		maxLength = 76
+	}
+	return &encoder{
+		w:             w,
+		status:        encodeNormal,
+		maxLineLength: maxLength,
+		lineLength:    0,
+		nbuf:          0,
+	}
+}
+
+func (e *encoder) Write(p []byte) (n int, err error) {
+	e.nbuf = 0
+	var b byte
+	for n, b = range p {
+		switch e.status {
+		case encodeNormal:
+			switch b {
+			case byte(' '):
+				fallthrough
+			case byte('\t'):
+				e.status = encodeSpace
+				e.last = b
+			case byte('\r'):
+				e.status = encodeReturn
+			case byte('\n'):
+				if err = e.push(byte('\r')); err != nil {
+					return
+				}
+				if err = e.push(byte('\n')); err != nil {
+					return
+				}
+			case byte('='):
+				if err = e.pushQuoted(byte('=')); err != nil {
+					return
+				}
+			default:
+				if err = e.pushCheck(b); err != nil {
+					return
+				}
+			}
+		case encodeSpace:
+			switch b {
+			case byte('\r'):
+				e.status = encodeSpaceReturn
+			case byte('\n'):
+				e.status = encodeNormal
+				if err = e.pushQuoted(e.last); err != nil {
+					return
+				}
+				if err = e.push('\r'); err != nil {
+					return
+				}
+				if err = e.push('\n'); err != nil {
+					return
+				}
+			default:
+				e.status = encodeNormal
+				if err = e.push(e.last); err != nil {
+					return
+				}
+				if err = e.pushCheck(b); err != nil {
+					return
+				}
+			}
+		case encodeSpaceReturn:
+			if b == byte('\n') {
+				if err = e.pushQuoted(e.last); err != nil {
+					return
+				}
+				if err = e.push('\r'); err != nil {
+					return
+				}
+				if err = e.push('\n'); err != nil {
+					return
+				}
+			} else {
+				if err = e.push(e.last); err != nil {
+					return
+				}
+				if err = e.pushQuoted(byte('\r')); err != nil {
+					return
+				}
+				if err = e.pushCheck(b); err != nil {
+					return
+				}
+			}
+		case encodeReturn:
+			if b == byte('\n') {
+				if err = e.push(byte('\r')); err != nil {
+					return
+				}
+				if err = e.push(byte('\n')); err != nil {
+					return
+				}
+			} else {
+				if err = e.pushQuoted(byte('\r')); err != nil {
+					return
+				}
+				if err = e.pushCheck(b); err != nil {
+					return
+				}
+			}
+		}
+	}
+	_, err = e.w.Write(e.buf[:e.nbuf])
+	return
+}
+
+func (e *encoder) Close() error {
+	return nil
+}
+
+func (e *encoder) pushCheck(p byte) error {
+	if 33 <= p && p <= 126 {
+		return e.push(p)
+	}
+	return e.pushQuoted(p)
+}
+
+func (e *encoder) push(p byte) error {
+	if p != byte('\r') && p != byte('\n') {
+		if (e.lineLength + 1) >= e.maxLineLength {
+			e.buf[e.nbuf] = byte('=')
+			e.nbuf++
+			e.buf[e.nbuf] = byte('\r')
+			e.nbuf++
+			e.buf[e.nbuf] = byte('\n')
+			e.nbuf++
+			e.lineLength = 0
+		}
+	} else {
+		e.lineLength = 0
+	}
+	e.buf[e.nbuf] = p
+	e.nbuf++
+	e.lineLength++
+	return e.checkAndSendBuffer()
+}
+
+func (e *encoder) pushQuoted(p byte) error {
+	if (e.lineLength + 3) >= e.maxLineLength {
+		e.push(byte('='))
+		e.push(byte('\r'))
+		e.push(byte('\n'))
+	}
+	if err := e.push(byte('=')); err != nil {
+		return err
+	}
+	for _, c := range hex(p) {
+		if err := e.push(byte(c)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (e *encoder) checkAndSendBuffer() error {
+	if e.nbuf >= maxBuf {
+		_, err := e.w.Write(e.buf[:])
+		if err != nil {
+			return err
+		}
+		e.nbuf = 0
+	}
+	return nil
 }
--- quoted_printable_test.go
+++ quoted_printable_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 type unHexData struct {
-	in byte
+	in  byte
 	out byte
-	ok bool
+	ok  bool
 }
 
 func TestUnHex(t *testing.T) {
-	datas := []unHexData{
+	datas := [...]unHexData{
 		{byte('0'), 0, true},
 		{byte('6'), 6, true},
 		{byte('a'), 10, true},
@@ -23,23 +23,42 @@ func TestUnHex(t *testing.T) {
 		{byte('y'), 0, false},
 		{byte('Y'), 0, false},
 		{byte('$'), 0, false},
-	}	
+	}
 	for _, d := range datas {
-		got, ok := UnHex(d.in)
+		got, ok := unHex(d.in)
 		if ok != d.ok {
 			t.Errorf("convert %d(%c) should be %v, got: %v", d.in, rune(d.in), d.ok, ok)
 		}
 		if ok && got != d.out {
-			t.Errorf("convert %d(%c) should get %d(%c), got: %d(%c)", 
+			t.Errorf("convert %d(%c) should get %d(%c), got: %d(%c)",
 				d.in, rune(d.in), d.out, rune(d.out), got, rune(got))
+		}
+	}
+}
+
+type hexData struct {
+	in  byte
+	out [2]byte
+}
+
+func TestHex(t *testing.T) {
+	datas := [...]hexData{
+		{byte('='), [2]byte{byte('3'), byte('D')}},
+		{byte(' '), [2]byte{byte('2'), byte('0')}},
+		{byte('\r'), [2]byte{byte('0'), byte('D')}},
+	}
+	for _, d := range datas {
+		got := hex(d.in)
+		if got != d.out {
+			t.Errorf("convert %d(%c) should be %v, got: %v", d.in, rune(d.in), d.out, got)
 		}
 	}
 }
 
 func TestQuotedPrintableDecode(t *testing.T) {
 	{
-		str := "If you believe that truth=3Dbeauty, then surely =\nmathematics is the most beautiful branch of philosophy."
-		expect := "If you believe that truth=beauty, then surely mathematics is the most beautiful branch of philosophy."
+		str := "If you believe that truth=3Dbeauty, then surely =\nmathematics is the most beautiful branch of phil=\nosophy.=20\nabc"
+		expect := "If you believe that truth=beauty, then surely mathematics is the most beautiful branch of philosophy. \nabc"
 		reader := NewQuotedPrintableDecoder(bytes.NewBufferString(str))
 		got, _ := ioutil.ReadAll(reader)
 		if string(got) != expect {
@@ -48,13 +67,26 @@ func TestQuotedPrintableDecode(t *testing.T) {
 	}
 
 	{
-		str := "If you believe that truth=3Dbeauty, then surely =\r\nmathematics is the most beautiful branch of philosophy."
-		expect := "If you believe that truth=beauty, then surely mathematics is the most beautiful branch of philosophy."
+		str := "If you believe that truth=3Dbeauty, then surely =\r\nmathematics is the most beautiful branch of phil=\r\nosophy.=20\r\nabc"
+		expect := "If you believe that truth=beauty, then surely mathematics is the most beautiful branch of philosophy. \r\nabc"
 		reader := NewQuotedPrintableDecoder(bytes.NewBufferString(str))
 		got, err := ioutil.ReadAll(reader)
 		t.Log(err)
 		if string(got) != expect {
 			t.Errorf("Decode\n\texpect: %s\n\t   got: %s", expect, string(got))
+		}
+	}
+}
+
+func TestQuotedPrintableEncode(t *testing.T) {
+	{
+		input := []byte("If you believe that truth=beauty, then surely mathematics is the most beautiful branch of philosophy. \nabc")
+		expect := "If you believe that truth=3Dbeauty, then surely =\r\nmathematics is the most beautiful branch of phil=\r\nosophy.=20\r\nabc"
+		buf := bytes.NewBuffer(nil)
+		writer := NewQuotedPrintableEncoder(buf, 49)
+		_, _ = writer.Write(input)
+		if buf.String() != expect {
+			t.Errorf("Encode\n\texpect: %s\n\t   got: %s", expect, buf.String())
 		}
 	}
 }
