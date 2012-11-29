6b3dc1d7fb13dcd1c29ad15a84380e713dd0e81b
Googol Lee <googollee@gmail.com>
2012-11-29 12:40:25 +0800

    remove go-iconv package

--- encoded_word_test.go
+++ encoded_word_test.go
@@ -2,7 +2,6 @@ package encodingex
 
 import (
 	"testing"
-	"github.com/sloonz/go-iconv"
 )
 
 func TestEncodedWordDeocder(t *testing.T) {
@@ -10,7 +9,7 @@ func TestEncodedWordDeocder(t *testing.T) {
 		str := "=?iso-8859-1?Q?=A1Hola,_se=F1or!?="
 		expect := "¡Hola, señor!"
 		got, charset, _ := DecodeEncodedWord(str)
-		got, _ = iconv.Conv(got, "UTF-8", charset)
+		got, _ = Conv(got, "UTF-8", charset)
 		if got != expect {
 			t.Errorf("expect: %s, got: %s", expect, got)
 		}
@@ -20,7 +19,7 @@ func TestEncodedWordDeocder(t *testing.T) {
 		strs := []string{"=?GB2312?B?W1RMXSBwb25nYmFAZ29vZ2xlZ3JvdXBzLmNvbSC1xNWq0qogLSChsDEguPbW9w==?=", "=?GB2312?B?zOKhsdPQIDEguPbM+9fT?="}
 		expect := "[TL] pongba@googlegroups.com 的摘要 - “1 个主题”有 1 个帖子"
 		got, charset, _ := DecodeEncodedWordArray(strs)
-		got, _ = iconv.Conv(got, "UTF-8", charset)
+		got, _ = Conv(got, "UTF-8", charset)
 		if got != expect {
 			t.Errorf("expect: %s, got: %s", expect, got)
 		}
